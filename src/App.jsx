496f9efefe25b48747cfd71986f0e47c2919bf0c
Eric Winkelspecht <eric.winkelspecht@gmail.com>
2022-09-06 16:23:16 -0400

    add tablet and desktop style

--- src/App.jsx
+++ src/App.jsx
@@ -14,56 +14,60 @@ function App() {
 
   return (
     <>
-      <header id="header" className="grid">
-        <h1>THE PLANETS</h1>
-        <svg onClick={mobileNav} xmlns="http://www.w3.org/2000/svg" width="24" height="17"><g fill="#FFF" fill-rule="evenodd"><path d="M0 0h24v3H0zM0 7h24v3H0zM0 14h24v3H0z" /></g></svg>
-      </header>
-      <nav id='planet-nav' role='website navigation'>
-        <ul>
-          <li><div className="planet-icon mercury"></div><a href="#">Mercury</a><img className="chevron" src={chevron} alt="chevron" /></li>
-          <li><div className="planet-icon venus"></div><a href="#">Venus</a><img className="chevron" src={chevron} alt="chevron" /></li>
-          <li><div className="planet-icon earth"></div><a href="#">Earth</a><img className="chevron" src={chevron} alt="chevron" /></li>
-          <li><div className="planet-icon mars"></div><a href="#">Mars</a><img className="chevron" src={chevron} alt="chevron" /></li>
-          <li><div className="planet-icon jupiter"></div><a href="#">Jupiter</a><img className="chevron" src={chevron} alt="chevron" /></li>
-          <li><div className="planet-icon saturn"></div><a href="#">Saturn</a><img className="chevron" src={chevron} alt="chevron" /></li>
-          <li><div className="planet-icon uranus"></div><a href="#">Uranus</a><img className="chevron" src={chevron} alt="chevron" /></li>
-          <li><div className="planet-icon neptune"></div><a href="#">Neptune</a><img className="chevron" src={chevron} alt="chevron" /></li>
-        </ul>
-      </nav>
-      <section id="planet-info" className="grid">
-        <img src={planet} alt="planet earth" />
-        <article>
-          <h2>PLANET NAME</h2>
-          <p>Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest of all the Sun's planets. Mercury is one of four terrestrial planets in the Solar System, and is a rocky body like Earth.
-          </p>
-          <footer>Source: <a href="#">Wikipedia</a><img className="source" src={source} alt="arrow" /></footer>
-        </article>
-        <nav role='planet feature navigation'>
-          <ol role='list'>
-            <li><span className="hide">01</span> Overview</li>
-            <li><span className="hide">02</span> <span className="hide">Internal </span>Structure</li>
-            <li><span className="hide">03</span> Surface <span className="hide">Geology</span></li>
-          </ol>
+      <div className="desktop-wrapper">
+        <header id="header" className="grid">
+          <h1>THE PLANETS</h1>
+          <svg onClick={mobileNav} xmlns="http://www.w3.org/2000/svg" width="24" height="17"><g fill="#FFF" fill-rule="evenodd"><path d="M0 0h24v3H0zM0 7h24v3H0zM0 14h24v3H0z" /></g></svg>
+        </header>
+        <nav id='planet-nav' role='website navigation'>
+          <ul>
+            <li><div className="planet-icon mercury"></div><a href="#">Mercury</a><img className="chevron" src={chevron} alt="chevron" /></li>
+            <li><div className="planet-icon venus"></div><a href="#">Venus</a><img className="chevron" src={chevron} alt="chevron" /></li>
+            <li><div className="planet-icon earth"></div><a href="#">Earth</a><img className="chevron" src={chevron} alt="chevron" /></li>
+            <li><div className="planet-icon mars"></div><a href="#">Mars</a><img className="chevron" src={chevron} alt="chevron" /></li>
+            <li><div className="planet-icon jupiter"></div><a href="#">Jupiter</a><img className="chevron" src={chevron} alt="chevron" /></li>
+            <li><div className="planet-icon saturn"></div><a href="#">Saturn</a><img className="chevron" src={chevron} alt="chevron" /></li>
+            <li><div className="planet-icon uranus"></div><a href="#">Uranus</a><img className="chevron" src={chevron} alt="chevron" /></li>
+            <li><div className="planet-icon neptune"></div><a href="#">Neptune</a><img className="chevron" src={chevron} alt="chevron" /></li>
+          </ul>
         </nav>
-      </section>
-      <section id="planet-facts" className="grid">
-        <article>
-          <h3>Rotation Time</h3>
-          <p>XX.X DAYS</p>
-        </article>
-        <article>
-          <h3>Revolution Time</h3>
-          <p>XX.XX DAYS</p>
-        </article>
-        <article>
-          <h3>Radius</h3>
-          <p>X,XXXX KM</p>
-        </article>
-        <article>
-          <h3>Average Temp</h3>
-          <p>XXXd C</p>
-        </article>
-      </section>
+      </div>
+      <div className="wrapper">
+        <section id="planet-info" className="grid">
+          <img className="planet" src={planet} alt="planet earth" />
+          <article>
+            <h2>PLANET</h2>
+            <p>Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest of all the Sun's planets. Mercury is one of four terrestrial planets in the Solar System, and is a rocky body like Earth.
+            </p>
+            <footer>Source: <a href="#">Wikipedia</a><img className="source" src={source} alt="arrow" /></footer>
+          </article>
+          <nav role='planet feature navigation'>
+            <ol role='list'>
+              <li><span className="hide num">01</span> Overview</li>
+              <li><span className="hide num">02</span> <span className="hide">Internal </span>Structure</li>
+              <li><span className="hide num">03</span> Surface <span className="hide">Geology</span></li>
+            </ol>
+          </nav>
+        </section>
+        <section id="planet-facts" className="grid">
+          <article>
+            <h3>Rotation Time</h3>
+            <p>XX.X DAYS</p>
+          </article>
+          <article>
+            <h3>Revolution Time</h3>
+            <p>XX.XX DAYS</p>
+          </article>
+          <article>
+            <h3>Radius</h3>
+            <p>X,XXXX KM</p>
+          </article>
+          <article>
+            <h3>Average Temp</h3>
+            <p>XXXd C</p>
+          </article>
+        </section>
+      </div>
     </>
   )
 }

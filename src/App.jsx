b57ff102fa0f04baf1a4d709cad8773c8d633744
Eric Winkelspecht <eric.winkelspecht@gmail.com>
2022-09-07 15:26:42 -0400

    update logic as SPA

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,13 @@ import data from './assets/data.json'
 
 function App() {
 
-  // to set state of page to show planet information
+  // to set state of page, planet nav and planet info
   const [pageState, setPageState] = useState('overview')
+  const [navState, setNavState] = useState('0')
 
-  //to access the current url and pull the appropriate planet info from the .json file
-  const url = window.location.pathname.slice(1, 2)
-  const planetNum = url || '0'
-
+  //set page color to planet color variable in css
   const root = document.documentElement
-  root.style.setProperty('--clr-planet', `var(--clr-primary-${data[planetNum].name})`)
-  console.log(root)
-
+  root.style.setProperty('--clr-planet', `var(--clr-primary-${data[navState].name})`)
 
   //show/hide the mobile planet menu
   function mobileNav() {
@@ -21,7 +17,14 @@ function App() {
     nav.classList.toggle('show')
   }
 
-
+  //handle website navigation when changing planets, reset pageState. Close menu if in mobile
+  function handleNavChange(e) {
+    setNavState(e.target.id)
+    setPageState('overview')
+    if (window.innerWidth < 600) {
+      mobileNav()
+    }
+  }
 
   return (
     <>
@@ -32,30 +35,30 @@ function App() {
         </header>
         <nav id='planet-nav' role='website navigation'>
           <ul>
-            <li><div className="planet-icon mercury"></div><a className={planetNum === '0' ? 'active' : null} href="/0">Mercury</a><img className="chevron" src='/assets/icon-chevron.svg' alt="chevron" /></li>
-            <li><div className="planet-icon venus"></div><a className={planetNum === '1' ? 'active' : null} href="/1">Venus</a><img className="chevron" src='/assets/icon-chevron.svg' alt="chevron" /></li>
-            <li><div className="planet-icon earth"></div><a className={planetNum === '2' ? 'active' : null} href="/2">Earth</a><img className="chevron" src='/assets/icon-chevron.svg' alt="chevron" /></li>
-            <li><div className="planet-icon mars"></div><a className={planetNum === '3' ? 'active' : null} href="/3">Mars</a><img className="chevron" src='/assets/icon-chevron.svg' alt="chevron" /></li>
-            <li><div className="planet-icon jupiter"></div><a className={planetNum === '4' ? 'active' : null} href="/4">Jupiter</a><img className="chevron" src='/assets/icon-chevron.svg' alt="chevron" /></li>
-            <li><div className="planet-icon saturn"></div><a className={planetNum === '5' ? 'active' : null} href="/5">Saturn</a><img className="chevron" src='/assets/icon-chevron.svg' alt="chevron" /></li>
-            <li><div className="planet-icon uranus"></div><a className={planetNum === '6' ? 'active' : null} href="/6">Uranus</a><img className="chevron" src='/assets/icon-chevron.svg' alt="chevron" /></li>
-            <li><div className="planet-icon neptune"></div><a className={planetNum === '7' ? 'active' : null} href="/7">Neptune</a><img className="chevron" src='/assets/icon-chevron.svg' alt="chevron" /></li>
+            <li id="0" className={navState === '0' ? 'active' : null} onClick={(e) => handleNavChange(e)}><div className="planet-icon mercury"></div>Mercury<img className="chevron" src='/assets/icon-chevron.svg' alt="chevron" /></li>
+            <li id="1" className={navState === '1' ? 'active' : null} onClick={(e) => handleNavChange(e)}><div className="planet-icon venus"></div>Venus<img className="chevron" src='/assets/icon-chevron.svg' alt="chevron" /></li>
+            <li id="2" className={navState === '2' ? 'active' : null} onClick={(e) => handleNavChange(e)}><div className="planet-icon earth"></div>Earth<img className="chevron" src='/assets/icon-chevron.svg' alt="chevron" /></li>
+            <li id="3" className={navState === '3' ? 'active' : null} onClick={(e) => handleNavChange(e)}><div className="planet-icon mars"></div>Mars<img className="chevron" src='/assets/icon-chevron.svg' alt="chevron" /></li>
+            <li id="4" className={navState === '4' ? 'active' : null} onClick={(e) => handleNavChange(e)}><div className="planet-icon jupiter"></div>Jupiter<img className="chevron" src='/assets/icon-chevron.svg' alt="chevron" /></li>
+            <li id="5" className={navState === '5' ? 'active' : null} onClick={(e) => handleNavChange(e)}><div className="planet-icon saturn"></div>Saturn<img className="chevron" src='/assets/icon-chevron.svg' alt="chevron" /></li>
+            <li id="6" className={navState === '6' ? 'active' : null} onClick={(e) => handleNavChange(e)}><div className="planet-icon uranus"></div>Uranus<img className="chevron" src='/assets/icon-chevron.svg' alt="chevron" /></li>
+            <li id="7" className={navState === '7' ? 'active' : null} onClick={(e) => handleNavChange(e)}><div className="planet-icon neptune"></div>Neptune<img className="chevron" src='/assets/icon-chevron.svg' alt="chevron" /></li>
           </ul>
         </nav>
       </div>
       <div className="desktop-body-wrapper">
         <section id="planet-info" className="grid">
           <div className="image-wrapper">
-            <img className="planet" src={pageState === 'structure' ? data[planetNum].images.internal : data[planetNum].images.planet} alt={data[planetNum].name} />
-            {pageState === 'geology' ? <img className="geology" src={data[planetNum].images.geology} alt={`${data[planetNum].name} geology`} /> : null}
+            <img className="planet" src={pageState === 'structure' ? data[navState].images.internal : data[navState].images.planet} alt={data[navState].name} />
+            {pageState === 'geology' ? <img className="geology" src={data[navState].images.geology} alt={`${data[navState].name} geology`} /> : null}
           </div>
           <article>
-            <h2>{data[planetNum].name}</h2>
-            {pageState === 'overview' ? <p>{data[planetNum].overview.content}</p>
-              : pageState === 'structure' ? <p>{data[planetNum].structure.content} </p>
-                : <p>{data[planetNum].geology.content}</p>}
+            <h2>{data[navState].name}</h2>
+            {pageState === 'overview' ? <p>{data[navState].overview.content}</p>
+              : pageState === 'structure' ? <p>{data[navState].structure.content} </p>
+                : <p>{data[navState].geology.content}</p>}
 
-            <footer>Source: <a href={data[planetNum].overview.source} > Wikipedia</a><img className="source" src='/assets/icon-source.svg' alt="arrow" /></footer>
+            <footer>Source: <a href={data[navState].overview.source} > Wikipedia</a><img className="source" src='/assets/icon-source.svg' alt="arrow" /></footer>
           </article>
           <nav role='planet feature navigation'>
             <ol role='list'>
@@ -68,19 +71,19 @@ function App() {
         <section id="planet-facts" className="grid">
           <article>
             <h3>Rotation Time</h3>
-            <p>{data[planetNum].rotation}</p>
+            <p>{data[navState].rotation}</p>
           </article>
           <article>
             <h3>Revolution Time</h3>
-            <p>{data[planetNum].revolution}</p>
+            <p>{data[navState].revolution}</p>
           </article>
           <article>
             <h3>Radius</h3>
-            <p>{data[planetNum].radius}</p>
+            <p>{data[navState].radius}</p>
           </article>
           <article>
             <h3>Average Temp</h3>
-            <p>{data[planetNum].temperature}</p>
+            <p>{data[navState].temperature}</p>
           </article>
         </section>
       </div>
@@ -89,7 +92,3 @@ function App() {
 }
 
 export default App
-
-
-//dynamic color
-//set color in json?
